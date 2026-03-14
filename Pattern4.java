11d1fba382d5561771c241e36826b184f29a45ff
Saurabh.Kanade <73542577+mrsaurabhkanade@users.noreply.github.com>
2026-03-14 12:48:19 +0530

    Create Pattern4.java

---
+++ Pattern4.java
@@ -0,0 +1,24 @@
+package com.multigenesys.Test04;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+        int n = 4;
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int min = Math.min(Math.min(top, bottom), Math.min(left, right));
+
+                System.out.print((n - min) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
